718ed478bf7f07d3e23c97a6e082a214468d782e
Alex Birch <Birch-san@users.noreply.github.com>
2024-03-06 19:42:54 +0900

    more progress on products

--- script/play.py
+++ script/play.py
@@ -17,6 +17,7 @@ m1 = torch.tensor([[0.,    1.],
 s = m0 @ m1
 
 def get_exp(input: FloatTensor) -> IntTensor:
+  # TODO: provide a way for user to pass in an already-allocated buffer (frexp out parameter)
   _, exp = input.frexp()
   exp.sub_(1)
   return exp
@@ -51,15 +52,20 @@ emins: Dict[torch.dtype, int] = {
 }
 # float16 exponent range goes from 2^-14 to 2^15, encoded as 00001–11110 (1 to 30) with an offset of 15
 
-def edotv(input: FloatTensor, tensor: FloatTensor, *, out: Optional[IntTensor] = None, acc_dtype=torch.int16) -> IntTensor:
+def edotv(
+  input: FloatTensor,
+  tensor: FloatTensor,
+  *,
+  out: Optional[IntTensor] = None,
+  acc_dtype=torch.int16,
+  acc: Optional[IntTensor] = None,
+) -> IntTensor:
   assert input.is_floating_point()
   assert tensor.is_floating_point()
   assert input.ndim == 1
   assert tensor.ndim == 1
   assert input.shape == tensor.shape
   assert input.dtype in emaxs
-  # exp_i = get_exp(input)
-  # exp_t = get_exp(tensor)
   emin: int = emins[input.dtype]
   emax: int = emaxs[input.dtype]
   elems = input.size(-1)
@@ -86,13 +92,45 @@ def edotv(input: FloatTensor, tensor: FloatTensor, *, out: Optional[IntTensor] =
     assert out.is_signed()
     assert out.ndim == 1
     assert out.shape == input.shape
+  
+  if acc is None:
+    acc = input.new_zeros((2, torch.iinfo(acc_dtype).bits,), dtype=acc_dtype)
+  else:
+    assert acc.dtype == acc_dtype
+    assert acc.ndim == 2
+    assert acc.size(-1) == torch.iinfo(acc_dtype).bits
+    # one batch into which to accumulate positive products, another for negative
+    assert acc.size(-2) == 2
+    assert not acc.requires_grad
+    acc.zero_()
+  
+  # TODO: provide a way for user to pass in an already-allocated buffer for each of these
+  exp_i = get_exp(input)
+  exp_t = get_exp(tensor)
 
-  # sums = torch.add(input, tensor, out=out)
-  acc = input.new_zeros((torch.iinfo(acc_dtype).bits,), dtype=acc_dtype)
+  # TODO: provide a way for user to pass in an already-allocated buffer for each of these
+  sign_i = input.sign()
+  sign_t = tensor.sign()
+  # TODO: is there a bitwise way to do this that's cheaper? would be nice to be able to say no multiplication was used!
+  #       and beware: sign 0 exists too, but for our purposes we could simplify that to 1 if it helps at all
+  sign_prods = sign_i * sign_t
+
+  # TODO: this could probably use a smaller dtype. only needs to be able to fit min_product ≤ x ≤ max_product
+  dp_dtype = acc_dtype
+  # TODO: provide a way for user to pass in an already-allocated buffer
+  # e_prod = input.new_zeros((1,), dtype=dp_dtype)
+
   ix = input.size(-1) - 1
-  # 2^16 + 2^16 
   while ix >= 0:
+    # operands (for multiplication)
     o_i, o_t = input[ix], tensor[ix]
+    # exponents
+    e_i, e_t = exp_i[ix], exp_t[ix]
+    # exponent-space elementwise product
+    # TODO: sum exponents into re-usable buffer instead of re-allocating e_prod
+    e_prod = e_i + e_t
+    sign_prod = sign_prods[ix]
+    # TODO: add to the appropriate counter for this e_prod,sign_prod acc
     ix -= 1
 
 
